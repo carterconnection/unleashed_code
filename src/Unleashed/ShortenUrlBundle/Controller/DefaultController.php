1b4305c6526afaf1ebe8c7dd61e0edc2ed1b1bb3
ccarter <carterconnection21@gmail.com>
2014-12-08 21:13:59 -0500

    laid out controllers to be used and broke out html layout

--- src/Unleashed/ShortenUrlBundle/Controller/DefaultController.php
+++ src/Unleashed/ShortenUrlBundle/Controller/DefaultController.php
@@ -2,11 +2,18 @@
 
 namespace Unleashed\ShortenUrlBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
+use Unleashed\ShortenUrlBundle\Model\Urls;
+use Unleashed\ShortenUrlBundle\Model\UserByIp;
+use Unleashed\ShortenUrlBundle\Model\UrlsQuery;
+use Unleashed\ShortenUrlBundle\Model\UserByIpQuery;
 
 class DefaultController extends SuperController
 {
-    public function indexAction($name)
+    public function indexAction(Request $request, $name)
     {
         return $this->render(
             'UnleashedShortenUrlBundle:Default:index.html.twig'
@@ -14,5 +21,27 @@ class DefaultController extends SuperController
                 'name' => $name
             )
         );
+    }
+    
+    public function viewAction(Request $request)
+    {
+        $data = new \stdClass();
+        
+        return $this->render(
+            'UnleashedShortenUrlBundle:Default:view.html.twig'
+            , array(
+                'data' => $data
+            )
+        );
+    }
+    
+    public function redirectAction(Request $request)
+    {
+        return true;
+    }
+    
+    public function downloadAction(Request $request)
+    {
+        return true;
     }
 }
